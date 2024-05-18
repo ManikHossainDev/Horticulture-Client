0b963f56d09dfc3c3fe3280f9a8d5157cce0bcce
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-05-18 18:08:03 +0600

    parser add express and cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 const app:Application = express()
 
+// parser options for express and cors
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
