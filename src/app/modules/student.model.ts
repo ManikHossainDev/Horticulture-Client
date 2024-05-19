3404096007d9e35ad8d618033eb64b4e08a79e72
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-05-19 14:12:56 +0600

    Create Route , Service And Controller

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -4,7 +4,7 @@ import {
   LocalGuardian,
   Student,
   UserName,
-} from './student.interface';
+} from './student/student.interface';
 
 const userNameSchema = new Schema<UserName>({
   firstName: {
@@ -83,4 +83,4 @@ const studentSchema = new Schema<Student>({
   isActive: ['active', 'blocked'],
 });
 
-// export const StudentModel = model<Student>('Student', studentSchema);
+export const StudentModel = model<Student>('Student', studentSchema);
